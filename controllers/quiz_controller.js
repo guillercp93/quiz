c2fbc805a33bfc182566f9ebeae2b0a17bb4d5b0
Guiller <guillercp@hotmail.es>
2015-07-31 16:59:05 -0500

    Busqueda de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,8 +19,8 @@ exports.index = function(req, res) {
 	{
 		search = "%" + req.query.search.trim().replace(/\s+/g,"%") + "%";
 	}
-	console.log(search);
-	models.Quiz.findAll({where:["pregunta like ?", search.toLowerCase()], order: 'pregunta ASC'}).then(function(quizes) {
+
+	models.Quiz.findAll({where:["pregunta like ?", search], order: 'pregunta ASC'}).then(function(quizes) {
 		res.render('quizes/index', { quizes: quizes, errors: []});
 	}).catch(function(error) { next(error);});
 }
