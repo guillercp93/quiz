bc6b281fe626c6f150cdd9000f2c79dc11531032
Guiller <guillercp@hotmail.es>
2015-07-31 10:37:14 -0500

    Despliegue BD en Heroku

--- models/models.js
+++ models/models.js
@@ -1,13 +1,34 @@
 var path = require('path');
 
+//postgres DATABASE_URL = postgres://user:pass@host:port/database
+//SQLite DATABASE_URL = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/),
+	DB_name = (url[6] || null),
+	user = (url[2] || null),
+	pwd = (url[3] || null),
+	protocol = (url[1] || null),
+	dialect = (url[1] || null),
+	port = (url[5] || null),
+	host = (url[4] || null),
+	storage = process.env.DATABASE_STORAGE;
+
 //cargar modelo ORM
 var Sequelize = require('sequelize');
 
 //Usar BBDD SqLite
-var sequelize = new Sequelize(null, null, null, {dialect: 'sqlite', storage: 'quiz.sqlite'});
+var sequelize = new Sequelize(DB_name, user, pwd, {
+	dialect: protocol,
+	protocol: protocol,
+	port: port,
+	host: host,
+	storage: storage,	//solo SQLite (.env)
+	omitNull: true		//solo Postgres
+});
 
 //importar la definicion de la tabla quiz
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 //exportar definicion de tabla quiz
 exports.Quiz = Quiz;
 
