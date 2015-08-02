4a5cbe4a71d750a531fb2b216e4ef71337490ccc
Guiller <guillercp@hotmail.es>
2015-08-02 09:16:42 -0500

    Creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -43,6 +43,20 @@ exports.answer = function(req, res) {
 	});
 }
 
+//GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build({pregunta: "pregunta", respuesta: "Respuesta"});
+	res.render('quizes/new', {quiz: quiz});
+}
+
+//POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+		res.redirect("/quizes");
+	});
+}
+
 exports.author = function(req, res) {
 	res.render('author');
 }
